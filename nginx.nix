64cec11c5c254a95fcc4a51295a41af70f686bb5
Daniel Frank <git@danielfrank.net>
2022-01-22 17:44:23 +0100

    enable https

--- nginx.nix
+++ nginx.nix
@@ -9,8 +9,10 @@
     recommendedOptimisation = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
-    virtualHosts."stats" = {
+    virtualHosts."stats.hamburg.freifunk.net" = {
       default = true;
+      enableACME = true;
+      forceSSL = true;
       locations."/".proxyPass = "http://unix:${config.services.grafana.socket}:/";
     };
   };
