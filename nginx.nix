162a2138be8867a4cf1276c203f18d4acd33f720
Daniel Frank <git@danielfrank.net>
2022-01-22 17:57:23 +0100

    allow port 443

--- nginx.nix
+++ nginx.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  networking.firewall.allowedTCPPorts = [ 80 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
   services.nginx = {
     enable = true;
     #logError = "/dev/null";
